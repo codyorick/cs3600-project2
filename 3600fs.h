0fc8bb647e4cb85445853b1b3d684a2707eacb38
orick <orick@lethalenforcers.ccs.neu.edu>
2013-10-04 21:05:47 -0400

    commit

---
+++ 3600fs.h
@@ -0,0 +1,72 @@
+/*
+ * CS3600, Fall 2013
+ * Project 2 Starter Code
+ * (c) 2013 Alan Mislove
+ *
+ */
+
+#ifndef __3600FS_H__
+#define __3600FS_H__
+
+
+#endif
+
+// Our magic number!!!
+#define MAGICNUMBER 48151623
+
+// Block structures. These are copied from the spec
+
+// VCB struct
+// probably doesn't actually matter what size this is, just as long as it's <= 512
+typedef struct vcb_s {
+  // to identify the disk
+  int magic;                    // 4
+  
+  int blocksize;                // 4
+  int de_start;                 // 4
+  int de_length;                // 4
+  int fat_start;                // 4
+  int fat_length;               // 4
+  int db_start;                 // 4
+  
+  // metadata
+  uid_t user;                   // 4
+  gid_t group;                  // 4
+  mode_t mode;                  // 4
+  struct timespec access_time;  // 8
+  struct timespec modify_time;  // 8
+  struct timespec create_time;  // 8
+} vcb;                          // 64 bytes
+
+// Directory Entry struct
+// we want 512 to be divisible by this eventually; for now, we'll make it 512
+typedef struct dirent_s {
+  unsigned int valid;                 // 4
+  unsigned int first_block;           // 4
+  unsigned int size;                  // 4
+  uid_t user;                         // 4
+  gid_t group;                        // 4
+  mode_t mode;                        // 4
+  const struct timespec access_time;  // 8
+  const struct timespec modify_time;  // 8
+  const struct timespec create_time;  // 8
+  // has to be at least 27; we're at 48; 512 - 48 = 464
+  char name[464];                     // 464
+} dirent;                             // 512 bytes
+
+// FAT Entry struct
+// These numbers are "bit fields" meaning this struct will be 32 bits (4 bytes)
+// So, 128 of these can fit into a block (512/4 = 128)
+typedef struct fatent_s {
+  unsigned int used:1;
+  unsigned int eof:1;
+  unsigned int next:30;
+} fatent;
+
+
+
+
+
+
+
+
---
+++ 3600mkfs.c
@@ -0,0 +1,138 @@
+/*
+ * CS3600, Fall 2013
+ * Project 2 Starter Code
+ * (c) 2013 Alan Mislove
+ *
+ * This program is intended to format your disk file, and should be executed
+ * BEFORE any attempt is made to mount your file system.  It will not, however
+ * be called before every mount (you will call it manually when you format 
+ * your disk file).
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+
+#include "3600fs.h"
+#include "disk.h"
+
+void myformat(int size) {
+  // Do not touch or move this function
+  dcreate_connect();
+
+  /* 3600: FILL IN CODE HERE.  YOU SHOULD INITIALIZE ANY ON-DISK
+           STRUCTURES TO THEIR INITIAL VALUE, AS YOU ARE FORMATTING
+           A BLANK DISK.  YOUR DISK SHOULD BE size BLOCKS IN SIZE. */
+
+  /* 3600: AN EXAMPLE OF READING/WRITING TO THE DISK IS BELOW - YOU'LL
+           WANT TO REPLACE THE CODE BELOW WITH SOMETHING MEANINGFUL. */
+           
+  // We want (from spec):
+  // 1 VCB
+  // 100 dirents
+  // fatents + data blocks = (size - 101)
+  // # of fatents = (data blocks / 128)
+  
+  
+  // Since fatents are 4 bytes, we can fit 128 of them in one block
+  // BLOCKSIZE is defined in disk.c (which we are not to modify)
+  // structs are defined in 3600fs.h
+  
+  // Create the VCB
+  vcb myvcb;
+  myvcb.magic = MAGICNUMBER;
+  myvcb.blocksize = BLOCKSIZE;
+  myvcb.de_start = 1; // starts after the VCB
+  myvcb.de_length = 100; // we want 100 dirents
+  int rest = size - 101;
+  int numfat = (rest / 128) + 1; // add extra fat block since division will truncate, possibly resulting in insufficient fat blocks
+  myvcb.fat_start = 101; // starts after VCB and dirents
+  myvcb.fat_length = numfat;
+  myvcb.db_start = myvcb.fat_start + myvcb.fat_length;
+  myvcb.user = geteuid();
+  myvcb.group = getegid();
+  myvcb.mode = 0777;
+  struct timespec currtime; // get the time for the time fields
+  clock_gettime(CLOCK_REALTIME, &currtime);
+  myvcb.access_time = currtime;
+  myvcb.modify_time = currtime;
+  myvcb.create_time = currtime;
+  
+  // Create an empty dirent
+  dirent mydirent; 
+  mydirent.valid = 0;
+  mydirent.size = 0;
+  // rest of the fields won't matter until the file is created
+  
+  // create empty fatent
+  fatent myfatent;
+  myfatent.used = 0;
+  myfatent.eof = 0;
+
+  // first, create a zero-ed out array of memory  
+  char *tmp = (char *) malloc(BLOCKSIZE);
+  memset(tmp, 0, BLOCKSIZE);
+  // copy the VCB into this
+  memcpy(tmp, &myvcb, sizeof(vcb));
+  // write it to the disk
+  dwrite(0, tmp);
+  
+  // write dirents to disk
+  memset(tmp, 0, BLOCKSIZE);
+  memcpy(tmp, &mydirent, sizeof(dirent));
+  for(int i = 1; i < 101; i++) {
+    dwrite(i, tmp);
+  }
+  
+  // write fat blocks
+  // first we want to create a block of just fatents
+  memset(tmp, 0, BLOCKSIZE);
+  for(int i = 0; i < BLOCKSIZE; i = i+4) {
+    memcpy(tmp + i, &myfatent, sizeof(fatent));
+  }
+  // write that to the disk
+  for(int i = myvcb.fat_start; i < (myvcb.fat_start + myvcb.fat_length); i++) {
+    dwrite(i, tmp);
+  }
+  
+  // write empty blocks for the data
+  memset(tmp, 0, BLOCKSIZE);
+  for(int i = myvcb.db_start; i < size; i++) {
+    dwrite(i, tmp);
+  }
+
+  // voila! we now have a disk containing all zeros
+
+  // Do not touch or move this function
+  dunconnect();
+}
+
+
+
+
+
+
+
+
+// Don't touch stuff below this
+/**********************************************************************/
+
+int main(int argc, char** argv) {
+  // Do not touch this function
+  if (argc != 2) {
+    printf("Invalid number of arguments \n");
+    printf("usage: %s diskSizeInBlockSize\n", argv[0]);
+    return 1;
+  }
+
+  unsigned long size = atoi(argv[1]);
+  printf("Formatting the disk with size %lu \n", size);
+  myformat(size);
+}
